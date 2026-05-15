39dbe35529d711cb2c21fbc5cbfa7c27dc9cee7e
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-15 14:03:23 +0500

    Search

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -2,7 +2,7 @@
 
 const Search = () => {
   return (
-    <div>search</div>
+    <div><input type="search"  className="w-140 h-15 border-2 border-white/40 rounded-2xl text-white placeholder:text-white/50 pl-5" placeholder="Поиск по названию..."/></div>
   )
 }
 
