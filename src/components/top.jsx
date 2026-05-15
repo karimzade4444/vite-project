1d9fef79f1f31a69d3d96dda55213cb8449cfab2
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-15 13:57:12 +0500

    top

--- src/components/top.jsx
+++ src/components/top.jsx
@@ -1,9 +1,17 @@
 
 
 const Top = () => {
-  return (
-    <div>top</div>
-  )
+    return (
+        <div className=" flex justify-between items-center p-10">
+            <div>
+                <p className=" font-bold text-white text-2xl">МИНИ-CRUD (Классная Работа)</p>
+                <p className="text-gray-500">Поиск • Модальные окна • Карточки</p>
+            </div>
+            <div className="text-white flex justify-center items-center gap-10"><p className=" flex justify-center items-center gap-2" >Всего: <p>1</p></p> <button className=" w-30 h-10 bg-white rounded-2xl text-black">+ Добавить</button>
+            </div>
+
+        </div>
+    )
 }
 
 export default Top
