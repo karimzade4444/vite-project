75994375519766ab36cdd1dde48bdd661156383b
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-15 13:58:24 +0500

    search

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Top from "./components/top"
+import Search from "./components/search"
 
 function App (){
   return (
     <div className=" bg-black h-screen">
     <Top/>
+    <Search/>
     </div>
   )
 }
